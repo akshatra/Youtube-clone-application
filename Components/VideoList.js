89e6e414910af0376f5021792514f2048e6eaeeb
Akshatra <47113617+akshatra@users.noreply.github.com>
2019-06-09 23:47:26 +0530

    Add files via upload

---
+++ Components/VideoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoItem from './VideoItem';
+
+
+const VideoList = ( {videos, onVideoSelect} ) =>{
+	const RenderedList=video.map((video) => { 
+		return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />;
+	});
+
+	return <div className="ui relaxed divided list">{RenderedList}</div>;
+};
+
+export default VideoList;
---
+++ apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY="AIzaSyAvxBXnUkIKh8nV1ZqxYsW5XJT7-a3T9Go";
+
+export default axios.create({
+	baseURL: "https://www.googleapis.com/youtube/v3",
+	params: {
+		part: 'snippet',
+		maxResults: 10,
+		key: KEY		
+	}
+});
